3d7afd26315df2b03bbdb6333e763c9b30601756
pan.xinyuan <1851616111@qq.com>
2016-12-08 16:29:59 +0800

    add tls example

---
+++ tls/twoway/genCert.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+openssl genrsa -out server.key 2048
+openssl req -new -x509 -key server.key -out server.pem -days 3650
+
+openssl genrsa -out client.key 2048
+openssl req -new -x509 -key client.key -out client.pem -days 3650
