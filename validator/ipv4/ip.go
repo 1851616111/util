ec24118cb57eb13c30b98b0afeaaa075f49a192b
pan.xinyuan <1851616111@qq.com>
2017-01-05 15:38:50 +0800

    add string regexp validator and mobile number validator

--- validator/ipv4/ip.go
+++ validator/ipv4/ip.go
@@ -1,21 +1,15 @@
 package validator
 
 import (
+	"github.com/1851616111/util/validator"
 	"github.com/pkg/errors"
 	"regexp"
 )
 
 var Err_Format_Invalid error = errors.New("ip format invalid")
-var ipRegxp *regexp.Regexp = regexp.MustCompile(`^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$`)
+
+var ipRegexp *regexp.Regexp = regexp.MustCompile(`^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$`)
 
 func Validate(ip string) error {
-	if ip == "" {
-		return Err_Format_Invalid
-	}
-
-	if !ipRegxp.MatchString(ip) {
-		return Err_Format_Invalid
-	}
-
-	return nil
+	return validator.Validate(ipRegexp, ip)
 }
---
+++ validator/mobile/mobile_test.go
@@ -0,0 +1,21 @@
+package mobile
+
+import "testing"
+
+func TestValidate(t *testing.T) {
+	validMobiles := []string{"13800000000", "17744524444", "15668688888"}
+	invalidMobiles := []string{"138000000000", "1380000000a", "aaaaaaaaaaa"}
+
+	for _, mobile := range validMobiles {
+		if err := Validate(mobile); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, mobile := range invalidMobiles {
+		if err := Validate(mobile); err == nil {
+			t.Fatal(err)
+		}
+	}
+}
+
