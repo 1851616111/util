33fc0354ffea48f28d8ae6c10d6f80855f114b5c
pan.xinyuan <1851616111@qq.com>
2017-01-05 15:53:24 +0800

    add port validator

---
+++ validator/port/port.go
@@ -0,0 +1,15 @@
+package validator
+
+import (
+	"github.com/1851616111/util/validator"
+	"github.com/pkg/errors"
+	"regexp"
+)
+
+var Err_Format_Invalid error = errors.New("port format invalid")
+
+var portRegexp *regexp.Regexp = regexp.MustCompile(`^([1-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-5]{2}[0-3][0-5])$`)
+
+func Validate(ip string) error {
+	return validator.Validate(portRegexp, ip)
+}
---
+++ validator/port/port_test.go
@@ -0,0 +1,20 @@
+package validator
+
+import "testing"
+
+func Test_Validate(t *testing.T) {
+	validatePorts := []string{"22", "80", "1080", "65535"}
+	invalidatePorts := []string{"-1", "65536", "0", "1a"}
+
+	for _, port := range validatePorts {
+		if err := Validate(port); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, port := range invalidatePorts {
+		if err := Validate(port); err == nil {
+			t.Fatal(port, err)
+		}
+	}
+}
