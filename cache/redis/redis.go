724b1c0a00db19e3e1da2801a1e12de7ef09605c
pan.xinyuan <1851616111@qq.com>
2017-01-05 17:56:36 +0800

    refactor cache redis

--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -1,7 +1,7 @@
 package redis
 
 import (
-	"github.com/garyburd/redigo/redis"
+	rd "github.com/garyburd/redigo/redis"
 	"time"
 )
 
@@ -13,13 +13,13 @@ type Cache interface {
 }
 
 func CreatePool(server, auth string) Cache {
-	return &cache{pool: &redis.Pool{
+	return &cache{pool: &rd.Pool{
 		MaxIdle:     10,
 		MaxActive:   10,
 		Wait:        true,
 		IdleTimeout: 4 * time.Minute,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server)
+		Dial: func() (rd.Conn, error) {
+			c, err := rd.Dial("tcp", server)
 			if err != nil {
 				return nil, err
 			}
@@ -31,7 +31,7 @@ func CreatePool(server, auth string) Cache {
 			}
 			return c, err
 		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+		TestOnBorrow: func(c rd.Conn, t time.Time) error {
 			_, err := c.Do("PING")
 			return err
 		},
@@ -39,19 +39,19 @@ func CreatePool(server, auth string) Cache {
 }
 
 type cache struct {
-	pool *redis.Pool
+	pool *rd.Pool
 }
 
 func (p *cache) Get(key string) ([]byte, error) {
 	c := p.pool.Get()
 	defer c.Close()
-	return redis.Bytes(c.Do("GET", key))
+	return rd.Bytes(c.Do("GET", key))
 }
 
 func (p *cache) Set(key string, data []byte) (err error) {
 	c := p.pool.Get()
 	defer c.Close()
-	_, err = redis.Bytes(c.Do("SET", key, data))
+	_, err = rd.Bytes(c.Do("SET", key, data))
 
 	return
 }
