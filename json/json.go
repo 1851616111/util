59e8e68f29a9766e33470cdd97ee28121350d58f
pan.xinyuan <1851616111@qq.com>
2017-01-09 19:22:23 +0800

    add json util

---
+++ json/json.go
@@ -0,0 +1,37 @@
+package json
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+// ReadJSON reads JSON from stream.
+func ReadJSON(r io.Reader, js interface{}) error {
+	return json.NewDecoder(r).Decode(js)
+}
+
+// ReadJSONFromFile reads JSON from the file with given name.
+func ReadJSONFromFile(filename string, js interface{}) error {
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	return ReadJSON(file, js)
+}
+
+// WriteJSON writes JSON to the stream.
+func WriteJSON(w io.Writer, js interface{}) error {
+	return json.NewEncoder(w).Encode(js)
+}
+
+// WriteJSONFromFile writes JSON to the file with given name.
+func WriteJSONToFile(filename string, js interface{}) error {
+	file, err := os.Create(filename)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	return WriteJSON(file, js)
+}
---
+++ json/json_test.go
@@ -0,0 +1,15 @@
+package json
+
+import "testing"
+
+func TestWriteJSONToFile(t *testing.T) {
+	m := map[string]string{
+		"a":"123",
+		"b":"456",
+	}
+
+	if err := WriteJSONToFile("test.json", m); err != nil {
+		t.Fatal(err)
+	}
+}
+
