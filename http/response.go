c6afdbc2dbf18d7128cf91c2f2a445442a11bf1b
panxy <1851616111@qq.com>
2017-02-09 15:33:55 +0800

    add http response

--- http/response.go
+++ http/response.go
@@ -3,10 +3,16 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
 func Response(w http.ResponseWriter, header int, body interface{}) {
 	w.WriteHeader(header)
 	fmt.Fprintf(w, "%v", body)
 	return
+}
+
+func ResponseJson(w http.ResponseWriter, header int, body interface{}) error {
+	w.WriteHeader(header)
+	return json.NewEncoder(w).Encode(body)
 }
