378406cb3376a11e9954c398f549a3975d724bd5
pan.xinyuan <1851616111@qq.com>
2017-03-22 10:55:51 +0800

    strings Clip() 重构

--- strings/clip.go
+++ strings/clip.go
@@ -3,10 +3,14 @@ package strings
 import "strings"
 
 func Clip(s string, left, mid, right string) []string {
+	if left != "" {
+		s = strings.TrimLeft(s, left)
+	}
+
+	if right != "" {
+		s = strings.TrimRight(s, right)
+	}
+
 	s = strings.TrimSpace(s)
-
-	s = strings.TrimLeft(s, left)
-	s = strings.TrimRight(s, right)
-
 	return strings.Split(s, mid)
 }
--- strings/clip_test.go
+++ strings/clip_test.go
@@ -8,6 +8,19 @@ import (
 func TestClip(t *testing.T) {
 	target := `"{2,1,3}"`
 	if ret := Clip(target, "\"{", ",", "}\""); !reflect.DeepEqual(ret, []string{"2", "1", "3"}) {
-		t.Error(ret)
+		t.Fatal(ret)
+	}
+	target = `2,1,3`
+	if ret := Clip(target, ``, `,`, ``); !reflect.DeepEqual(ret, []string{"2", "1", "3"}) {
+		t.Fatal(ret)
+	}
+
+	target = `,,`
+	if ret := Clip(target, ``, `,`, ``); len(ret)!= 3 {
+		t.Fatal(ret)
+	}
+
+	if "" != `` {
+		t.Fatal()
 	}
 }
