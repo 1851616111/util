bc4d9a717b6c540b7ee6ccc3260be0346c88cb4d
panxy <1851616111@qq.com>
2017-03-23 10:04:40 +0800

    refactor clip param string to ptr string

--- strings/clip.go
+++ strings/clip.go
@@ -6,27 +6,39 @@ var DB_ARRAY_MID string = `^,^`
 var DB_ARRAY_PERFIX string = `[^[`
 var DB_ARRAY_SUFFIX string = `]^]`
 
-var DB_OBJECT_MID string = `^,^`
+var DB_OBJECT_MID string = `^:^`
 var DB_OBJECT_PERFIX string = `{^{`
 var DB_OBJECT_SUFFIX string = `}^}`
 
-func Clip(s string, left, mid, right string) []string {
+func Clip(s *string, left, mid, right string) []string {
+	if s == nil {
+		return  nil
+	}
+
+	result := strings.TrimSpace(*s)
+
 	if left != "" {
-		s = strings.TrimLeft(s, left)
+		result = strings.TrimLeft(result, left)
 	}
 
 	if right != "" {
-		s = strings.TrimRight(s, right)
+		result = strings.TrimRight(result, right)
 	}
 
-	s = strings.TrimSpace(s)
-	return strings.Split(s, mid)
+	return strings.Split(result, mid)
 }
 
-func ClipDBArray(s string) []string {
+func ClipDBArray(s *string) []string {
+	if s == nil {
+		return nil
+	}
+
 	return Clip(s, DB_ARRAY_PERFIX, DB_ARRAY_MID, DB_ARRAY_SUFFIX)
 }
 
-func ClipDBObject(s string) []string {
+func ClipDBObject(s *string) []string {
+	if s == nil {
+		return nil
+	}
 	return Clip(s, DB_OBJECT_PERFIX, DB_OBJECT_MID, DB_OBJECT_SUFFIX)
 }
