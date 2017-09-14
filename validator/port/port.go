3c912081ee46f21546b2d586fd2117b52f574072
pan.xinyuan <1851616111@qq.com>
2017-09-14 16:06:02 +0800

    add validate for port

--- validator/port/port.go
+++ validator/port/port.go
@@ -10,6 +10,14 @@ var Err_Format_Invalid error = errors.New("port format invalid")
 
 var portRegexp *regexp.Regexp = regexp.MustCompile(`^([1-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-5]{2}[0-3][0-5])$`)
 
-func Validate(ip string) error {
-	return validator.Validate(portRegexp, ip)
+func Validate(port string) error {
+	return validator.Validate(portRegexp, port)
+}
+
+func ValidateInt(port int) error {
+	if port < 0 || port > 65535 {
+		return Err_Format_Invalid
+	}
+
+	return  nil
 }
