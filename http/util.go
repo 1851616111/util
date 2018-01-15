963c56794ecbf5032b687ea9564950d26bc26832
panxy <1851616111@qq.com>
2018-01-15 19:00:01 +0800

    add http util

--- http/util.go
+++ http/util.go
@@ -1,13 +1,27 @@
 package http
 
-import "net/http"
+import (
+	"time"
+	"net/http"
+	"crypto/tls"
+)
+
+var clientTransport = &http.Transport{
+	MaxIdleConns:        500,
+	MaxIdleConnsPerHost: 100,
+	IdleConnTimeout:     time.Minute * 10,
+	TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true,
+	},
+}
 
 func Send(spec *HttpSpec) (*http.Response, error) {
 	req, err := NewRequest(spec)
 	if err != nil {
 		return nil, err
 	}
-
-	cli := http.Client{}
+	cli := http.Client{
+		Transport: clientTransport,
+	}
 	return cli.Do(req)
 }
