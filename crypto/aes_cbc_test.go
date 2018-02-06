1f3afb12d2d4de1edfe7c3e620b5a2b4369a1c46
panxy <1851616111@qq.com>
2018-02-06 19:40:22 +0800

    add aes cbc

---
+++ crypto/aes_cbc_test.go
@@ -0,0 +1,10 @@
+package crypto
+
+import (
+	"testing"
+)
+
+func TestAesEncrypt2_Encrypt(t *testing.T) {
+	testAes()
+	//R0NDomMrrgtdY87gWHM/Vg==
+}
---
+++ crypto/aes_test.go
@@ -0,0 +1,32 @@
+package crypto
+//
+//import (
+//	"log"
+//	"testing"
+//	//"encoding/base64"
+//)
+//
+//func TestAesEncrypt(t *testing.T) {
+//	/*
+//	*src 要加密的字符串
+//	*key 用来加密的密钥 密钥长度可以是128bit、192bit、256bit中的任意一个
+//	*16位key对应128bit
+//	 */
+//
+//	 //res, err := base64.StdEncoding.DecodeString("l3vEgKemkQukpE81ctv7OCzKAZIlT7h35jj3p50b9XM=")
+//	 //if err != nil {
+//	 //	t.Fatal(err)
+//	 //}
+//	 //
+//	 //log.Println("-------->", string(res))
+//
+//	//src := ([]byte)("F3171953FF0612A30D81BC060BF1C8F3ADCD5C76C83C0049DB00B6687119E027")
+//	key := "Iod9aG3Df3Xde9Nb"
+//	//
+//	crypted := AesEncrypt("123", key)
+//	log.Println("------>", string(crypted))
+//	//data := AesDecrypt(res, []byte("Iod9aG3Df3Xde9Nb"))
+//	Base64URLDecode("39W7dWTd_SBOCM8UbnG6qA")
+//	//
+//	log.Printf("------->%s\n", string(crypted))
+//}
